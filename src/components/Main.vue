7729caf5d31d41e6eab1c34a6ce658a0b1e9696f
Ivanpepp <nastroivan3@gmail.com>
2021-10-05 10:24:36 +0200

    star-fixed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,7 @@
                     <h1 >FILM:</h1>
                     <ul class="d-flex flex-wrap pt-3 mx-2">
                         <li class=" mb-5 d-flex flex-wrap mx-auto" v-for="(element, index) in filterFilm" :key='index'>
-                             <img id="banner-image"  :src="pathGenerator(element)" :alt="element.title">
+                             <img id="banner-image"  class="image-fluid" :src="pathGenerator(element)" :alt="element.title">
                              <div class="content ">
                                  <h5>Titolo</h5> <span> {{element.title}}</span>
                                  <h5>Titolo originale:</h5> <span>{{element.original_title}} </span>
@@ -14,7 +14,7 @@
                               
                                 <img v-if="element.original_language === 'en'" src="../assets/img/Flag_of_the_United_Kingdom_(3-5).svg.png" alt="United Kingdom flag" class="flag"> 
                                 <img v-if="element.original_language === 'it'"  src="../assets/img/64px-Flag_of_Italy.svg.png" alt="Italy flag" class="flag">
-                                <h5>Voto: </h5> <span><i v-for="(star,counter) in stars " :key='counter' :class="starHighLighter(element,counter)" class="fas fa-star">{{star}}</i></span>
+                                <h5>Voto: </h5> {{element.vote_average}} <span><i v-for="(star,counter) in 5 " :key='counter' :class="starHighLighter(element,counter)" class="fas fa-star"></i></span>
                               
                              </div>
                             
@@ -26,7 +26,7 @@
                     <h1 >SERIE TV:</h1>
                     <ul class="d-flex flex-wrap pt-3 mx-2">
                         <li class=" mb-5 d-flex flex-wrap mx-auto" v-for="(element, index) in filterTVs" :key='index'>
-                            <img id="banner-image"  :src="pathGenerator(element)" :alt="element.name">
+                            <img id="banner-image"  class="image-fluid" :src="pathGenerator(element)" :alt="element.name">
                             <div class="content ">
                                  <h5>Titolo</h5> <span> {{element.name}}</span>
                                  <h5>Titolo originale:</h5> <span>{{element.original_name}} </span>
@@ -34,7 +34,7 @@
                               
                                 <img v-if="element.original_language === 'en'" src="../assets/img/Flag_of_the_United_Kingdom_(3-5).svg.png" alt="United Kingdom flag" class="flag"> 
                                 <img v-if="element.original_language === 'it'"  src="../assets/img/64px-Flag_of_Italy.svg.png" alt="Italy flag" class="flag">
-                                <h5>Voto: </h5> <span><i v-for="(star,counter) in stars " :key='counter' :class="starHighLighter(element,counter)" class="fas fa-star">{{star}}</i></span>
+                                <h5>Voto: </h5> <span><i v-for="(star,counter) in 5 " :key='counter' :class="starHighLighter(element,counter)" class="fas fa-star"></i></span>
                               
                              </div>
                            <!--  {{element.name }} - titolo originale: {{element.original_name}} 
@@ -69,7 +69,7 @@ export default {
     },
     data: function(){
         return{
-            stars:['','','','',''],
+            
         }
     },
     computed:{
@@ -94,8 +94,9 @@ export default {
             return  `https://image.tmdb.org/t/p/w342${element.poster_path}`;
         },
         starHighLighter: function(element,counter){         
-            let k = parseInt(element.vote_average / 2 -1 );     
-            if (counter <= k){
+            let k = Math.ceil(element.vote_average / 2);   
+            console.log(k)  
+            if (counter < k){
                 return 'active';
             }else{
                 return 'inActive'
